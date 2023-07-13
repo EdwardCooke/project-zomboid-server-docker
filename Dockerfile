39ea7ee7b113d1787924717a039b0acd7cfe5a66
Edward Cooke <edward@frakkingsweet.com>
2023-07-12 23:53:42 -0600

    Made it work in my environment the way I wanted

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 ###########################################################
 # Dockerfile that builds a CSGO Gameserver
 ###########################################################
-FROM cm2network/steamcmd:root
+FROM cm2network/steamcmd:steam
 
 LABEL maintainer="daniel.carrasco@electrosoftcloud.com"
 
 ENV STEAMAPPID 380870
 ENV STEAMAPP pz
-ENV STEAMAPPDIR "${HOMEDIR}/${STEAMAPP}-dedicated"
+ENV STEAMAPPDIR "${HOMEDIR}/${STEAMAPP}-dedicated/pz"
 
+USER root
 # Install required packages
 RUN apt-get update \
     && apt-get install -y --no-install-recommends --no-install-suggests \
@@ -16,19 +17,9 @@ RUN apt-get update \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
-# Download the Project Zomboid dedicated server app using the steamcmd app
-# Set the entry point file permissions
-RUN set -x \
-  && mkdir -p "${STEAMAPPDIR}" \
-  && chown -R "${USER}:${USER}" "${STEAMAPPDIR}" \
-  && bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
-                                    +login anonymous \
-                                    +app_update "${STEAMAPPID}" validate \
-                                    +quit
-
 # Copy the entry point file
-COPY --chown=${USER}:${USER} scripts/entry.sh /server/scripts/entry.sh
-RUN chmod 550 /server/scripts/entry.sh
+COPY --chown=root:root --chmod=555 scripts/entry.sh /server/scripts/entry.sh
+USER steam
 
 # Create required folders to keep their permissions on mount
 RUN mkdir -p "${HOMEDIR}/Zomboid"
--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+echo "Servername: ${SERVERNAME}"
+
+# Make sure the steam directory exists
+mkdir -p "${STEAMAPPDIR}"
+echo "App Dir: $STEAMAPPDIR"
 
 cd ${STEAMAPPDIR}
 
-#####################################
-#                                   #
-# Force an update if the env is set #
-#                                   #
-#####################################
+######################
+#                    #
+# Update if required #
+#                    #
+######################
 
-if [ "${FORCEUPDATE}" == "1" ]; then
-  echo "FORCEUPDATE variable is set, so the server will be updated right now"
-  bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +app_update "${STEAMAPPID}" validate +quit
-fi
-
+echo "Doing upgrade"
+bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +app_update "${STEAMAPPID}" validate +quit
+echo "Done"
 
 ######################################
 #                                    #
@@ -23,11 +26,13 @@ ARGS=""
 
 # Set the server memory. Units are accepted (1024m=1Gig, 2048m=2Gig, 4096m=4Gig): Example: 1024m
 if [ -n "${MEMORY}" ]; then
+  echo "Setting memory to ${MEMORY}"
   ARGS="${ARGS} -Xmx${MEMORY} -Xms${MEMORY}"
 fi
 
 # Option to perform a Soft Reset
 if [ "${SOFTRESET}" == "1" ] || [ "${SOFTRESET,,}" == "true" ]; then
+  echo "Doing a soft reset"
   ARGS="${ARGS} -Dsoftreset"
 fi
 
@@ -37,6 +42,7 @@ ARGS="${ARGS} -- "
 # Disables Steam integration on server.
 # - Default: Enabled
 if [ "${NOSTEAM}" == "1" ] || [ "${NOSTEAM,,}" == "true" ]; then
+  echo "Not allowing steam users"
   ARGS="${ARGS} -nosteam"
 fi
 
@@ -44,6 +50,7 @@ fi
 # - Default: ~/Zomboid
 # - Example: /server/Zomboid/data
 if [ -n "${CACHEDIR}" ]; then
+  echo "Setting cachedir to ${CACHEDIR}"
   ARGS="${ARGS} -cachedir=${CACHEDIR}"
 fi
 
@@ -51,12 +58,14 @@ fi
 # - Default: workshop,steam,mods
 # - Example: mods,steam
 if [ -n "${MODFOLDERS}" ]; then
+  echo "Setting modfolders to ${MODFOLDERS}"
   ARGS="${ARGS} -modfolders ${MODFOLDERS}"
 fi
 
 # Launches the game in debug mode.
 # - Default: Disabled
 if [ "${DEBUG}" == "1" ] || [ "${DEBUG,,}" == "true" ]; then
+  echo "Adding debug"
   ARGS="${ARGS} -debug"
 fi
 
@@ -65,18 +74,22 @@ fi
 # Once is launched and data is created, then can be removed without problem.
 # Is recommended to remove it, because the server logs the arguments in clear text, so Admin password will be sent to log in every startup.
 if [ -n "${ADMINPASSWORD}" ]; then
+  echo "Setting admin password to ***"
   ARGS="${ARGS} -adminpassword ${ADMINPASSWORD}"
 fi
 
 # Server password
 if [ -n "${PASSWORD}" ]; then
+  echo "Setting server password to ***"
   ARGS="${ARGS} -password ${PASSWORD}"
 fi
 
 # You can choose a different servername by using this option when starting the server.
 if [ -n "${SERVERNAME}" ]; then
+  echo "Setting servername to ${SERVERNAME}"
   ARGS="${ARGS} -servername ${SERVERNAME}"
 else
+  echo "Server name is not set ${SERVERNAME}, defaulting to servertest"
   # If not servername is set, use the default name in the next step
   SERVERNAME="servertest"
 fi
@@ -141,12 +154,11 @@ if [ -n "${WORKSHOP_IDS}" ]; then
 	sed -i "s/WorkshopItems=.*/WorkshopItems=${WORKSHOP_IDS}/" "${HOMEDIR}/Zomboid/Server/${SERVERNAME}.ini"
 fi
 
-
 # Fix to a bug in start-server.sh that causes to no preload a library:
 # ERROR: ld.so: object 'libjsig.so' from LD_PRELOAD cannot be preloaded (cannot open shared object file): ignored.
 export LD_LIBRARY_PATH="${STEAMAPPDIR}/jre64/lib:${LD_LIBRARY_PATH}"
 
-## Fix the permissions in the data and workshop folders
-chown -R 1000:1000 /home/steam/pz-dedicated/steamapps/workshop /home/steam/Zomboid
+cd ${STEAMAPPDIR}
+./start-server.sh $ARGS
 
-su - steam -c "export LD_LIBRARY_PATH=\"${STEAMAPPDIR}/jre64/lib:${LD_LIBRARY_PATH}\" && cd ${STEAMAPPDIR} && pwd && ./start-server.sh ${ARGS}"
+sleep 600
